919bac44f218c9316f0e32f2a995901a87735df7
Fabrice Laporte <kraymer@gmail.com>
2022-05-27 10:19:47 +0200

    setup: switch to alabaster docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,7 @@
-extensions = ["myst_parser"]
+import alabaster
+
+html_theme = "alabaster"
+extensions = ["alabaster", "myst_parser"]
 
 source_suffix = [".rst", ".md"]
 
@@ -6,9 +9,22 @@ master_doc = "index"
 project = "pawnhub"
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ["static"]
+html_static_path = ["_static"]
 
 # These paths are relative to html_static_path
 html_css_files = [
     "css/custom.css",
 ]
+
+html_theme_options = {
+    "logo": "logo.png",
+    "github_user": "kraymer",
+    "github_repo": "pawnhub",
+}
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    "**": [
+        "sidebarintro.html",
+    ]
+}
