ff13ad640b1d03691d9712b43066eabdf3de48ee
Fabrice Laporte <kraymer@gmail.com>
2022-07-05 22:33:24 +0200

    tests: add 2 unit tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,23 @@
-from pawnhub import numerize_turns
+from dataclasses import dataclass
+import pawnhub
+
+
+@dataclass
+class Game:
+    white: bool
 
 
 def test_numerize_turns():
-    assert numerize_turns(["e4", "e5"]) == ["1.", "e4", "e5"]
+    assert pawnhub.numerize_turns(["e4", "e5"]) == ["1.", "e4", "e5"]
+
+
+def test_sanitize_prefix():
+    assert pawnhub.sanitize_prefix(["1.", "e4", "e5", "2."]) == ["1.", "e4", "e5"]
+
+
+def test_status_first_move_out_line():
+    game = Game(white=False)
+    assert (
+        pawnhub.status_first_move_out_line(game, ["1.", "d4", "Nf6", "2.", "Bf4", "c5"])
+        == pawnhub.MoveStatus.OUT_OF_REP
+    )
