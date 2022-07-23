375d1d89a94176dc9fe69ef9037a7c971d5a0978
Fabrice <kraymer+github@gmail.com>
2022-07-23 11:00:15 +0200

    tests: add test_find_repertoire_line (#4)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,16 @@ import pawnhub
 
 @dataclass
 class Game:
-    white: bool
+    white: bool = False
+    moves: list = "d4 Nf6 Bf4 c5 e3 cxd4"
+
+
+repertoire = {
+    False: {
+        "d4 Nf6 Bf4 c5 d5 b5": "http://example1.com",
+        "d4 Nf6 Bf4 c5 e3 b5": "http://example2.com",
+    }
+}
 
 
 def test_numerize_turns():
@@ -16,8 +25,12 @@ def test_sanitize_prefix():
 
 
 def test_status_first_move_out_line():
-    game = Game(white=False)
+    game = Game()
     assert (
         pawnhub.status_first_move_out_line(game, ["1.", "d4", "Nf6", "2.", "Bf4", "c5"])
         == pawnhub.MoveStatus.OUT_OF_REP
     )
+
+
+def test_find_repertoire_line():
+    assert pawnhub.find_repertoire_line(Game(), repertoire) == "d4 Nf6 Bf4 c5 e3 b5"
